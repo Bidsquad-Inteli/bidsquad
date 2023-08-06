d6dedbbd9a067df156f59bc654959748ba21a594
Lemos1347 <henrique.matias@sou.inteli.edu.br>
2023-08-06 20:06:59 -0300

    feat(auctions): sending input to blockchain

--- frontend/src/app/auctions/page.tsx
+++ frontend/src/app/auctions/page.tsx
@@ -8,79 +8,82 @@ import Link from "next/link";
 import path from "path";
 
 const getAuctions = async () => {
-    const DEFAULT_URL = process.env.NEXT_PUBLIC_API_URL || "http://localhost:5005/inspect";
+  const DEFAULT_URL =
+    process.env.NEXT_PUBLIC_API_URL || "http://localhost:5005/inspect";
 
-    const DEFAULT_PAYLOAD = "auctions";
+  const DEFAULT_PAYLOAD = "auctions";
 
-    const response = await fetch(path.join(DEFAULT_URL, DEFAULT_PAYLOAD));
+  const response = await fetch(path.join(DEFAULT_URL, DEFAULT_PAYLOAD));
 
-    if (response.status != 200) {
-        return {
-            redirect: {
-                destination: "/",
-                permanent: false,
-            },
-        };
-    }
+  if (response.status != 200) {
+    return {
+      redirect: {
+        destination: "/",
+        permanent: false,
+      },
+    };
+  }
 
-    const res = await response.json();
+  const res = await response.json();
 
-    console.log(`Inspect status: ${JSON.stringify(res.status)}`);
-    console.log(`Input count: ${JSON.stringify(res.processed_input_count)}`);
-    console.log(`Reports:`);
+  console.log(`Inspect status: ${JSON.stringify(res.status)}`);
+  console.log(`Input count: ${JSON.stringify(res.processed_input_count)}`);
+  console.log(`Reports:`);
 
-    let auctions;
-    console.log(res.reports);
-    for (let i in res.reports) {
-        let payload = res.reports[i].payload;
-        auctions = JSON.parse(`${hex2str(payload)}`);
-    }
-    if (res.exception_payload) {
-        let payload = res.exception_payload;
-        console.log(`Exception payload: ${hex2str(payload)}`);
-    }
+  let auctions;
+  console.log(res.reports);
+  for (let i in res.reports) {
+    let payload = res.reports[i].payload;
+    auctions = JSON.parse(`${hex2str(payload)}`);
+  }
+  if (res.exception_payload) {
+    let payload = res.exception_payload;
+    console.log(`Exception payload: ${hex2str(payload)}`);
+  }
 
-    return auctions;
+  return auctions;
 };
 
 interface Auction {
-    id: string;
-    state: number;
-    creator: string;
-    carbonCredit: number;
-    satteliteImageUrl: string;
-    title: string;
-    description: string;
-    startDate: string;
-    endDate: string;
+  id: string;
+  state: number;
+  creator: string;
+  carbonCredit: number;
+  satteliteImageUrl: string;
+  title: string;
+  description: string;
+  startDate: string;
+  endDate: string;
 }
 
 const Auctions: React.FC = async () => {
-    const auctions: Auction[] = await getAuctions();
+  const auctions: Auction[] = await getAuctions();
 
-    return (
-        <Layout title={"Auctions"}>
-            <div className="w-auto flex flex-col content-start items-center mt-6 ml-6">
-                <h1 className="w-auto text-3xl self-start font-bold">Live Auctions 🔥</h1>
-                <label className="w-auto text-sm self-start font-bold text-gray-400">
-                    Enjoy! The latest hot auctions
-                </label>
-            </div>
+  return (
+    <Layout title={"Auctions"}>
+      <div className="w-auto flex flex-col content-start items-center mt-6 ml-6">
+        <h1 className="w-auto text-3xl self-start font-bold">
+          Live Auctions 🔥
+        </h1>
+        <label className="w-auto text-sm self-start font-bold text-gray-400">
+          Enjoy! The latest hot auctions
+        </label>
+      </div>
 
-            <div className="w-full mt-12 flex flex-wrap justify-center items-center gap-4">
-                {auctions.map((auction, index) => (
-                    <AuctionCard key={index} {...auction} />
-                ))}
-            </div>
+      <div className="w-full mt-12 flex flex-wrap justify-center items-center gap-4">
+        {auctions.map((auction, index) => (
+          <AuctionCard key={index} {...auction} />
+        ))}
+      </div>
 
-            <Link
-                href={"/auctions/new"}
-                className="fixed flex justify-center items-center bg-primary bottom-[50px] right-[50px] w-[50px] h-[50px] rounded-[50%]"
-            >
-                <AiOutlinePlus size={30} color="white" />
-            </Link>
-        </Layout>
-    );
+      <Link
+        href={"/auctions/new"}
+        className="fixed flex justify-center items-center bg-primary bottom-[50px] right-[50px] w-[50px] h-[50px] rounded-[50%]"
+      >
+        <AiOutlinePlus size={30} color="white" />
+      </Link>
+    </Layout>
+  );
 };
 
 export default Auctions;
---
+++ frontend/src/utils/send_data.ts
@@ -0,0 +1,32 @@
+import { InputBox__factory } from "@cartesi/rollups";
+import { ethers } from "ethers";
+
+const INPUTBOX_ADDRESS = "0x5a723220579C0DCb8C9253E6b4c62e572E379945";
+
+const DAPP_ADDRESS = "0x142105FC8dA71191b3a13C738Ba0cF4BC33325e2";
+
+export async function handleSubmit(data) {
+  // Start a connection
+  const provider = new ethers.providers.Web3Provider((window as any).ethereum);
+  const signer = provider.getSigner();
+
+  // Instantiate the InputBox contract
+  const inputBox = InputBox__factory.connect(INPUTBOX_ADDRESS, signer);
+
+  // Encode the input
+  const inputBytes = ethers.utils.isBytesLike(data)
+    ? data
+    : ethers.utils.toUtf8Bytes(data);
+
+  // Send the transaction
+  const tx = await inputBox.addInput(DAPP_ADDRESS, inputBytes);
+  console.log(`transaction: ${tx.hash}`);
+
+  // Wait for confirmation
+  console.log("waiting for confirmation...");
+  const receipt = await tx.wait(1);
+
+  // Search for the InputAdded event
+  const event = receipt.events?.find((e) => e.event === "InputAdded");
+  return event;
+}
