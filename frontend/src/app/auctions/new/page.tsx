12ee27fd40942f7833e872499ee1700021b8516a
pepehaggeb <99206621+PedroHaggeBaptista@users.noreply.github.com>
2023-08-06 23:50:57 -0300

    Atts Create Auction Page

--- frontend/src/app/auctions/new/page.tsx
+++ frontend/src/app/auctions/new/page.tsx
@@ -1,9 +1,7 @@
 "use client";
 //@ts-nocheck
-
-"use client";
 import { Layout } from "@/components/Layout";
-import { SemipolarLoading } from "react-loadingg";
+import { SemipolarLoading, WaveTopBottomLoading } from "react-loadingg";
 
 import toast, { Toaster } from "react-hot-toast";
 
@@ -20,6 +18,9 @@ const NewAuction = () => {
   const [map, setMap] = useState("");
   const [isFirstPage, setIsFirstPage] = useState(true);
   const [isSecondPage, setIsSecondPage] = useState(false);
+  const [file, setFile] = useState(null);
+  const [base64, setBase64] = useState("");
+  const [ changingPage, setChangingPage ] = useState(false)
 
   function changeQnt(e: any, type: number) {
     e.preventDefault();
@@ -42,15 +43,7 @@ const NewAuction = () => {
     setCoordinates(aux);
   }
 
-  async function fetchImageAndConvertToBase64(url) {
-    // Fetch the image
-    const response = await fetch(url);
-
-    // Get the blob data of the image
-    const blob = await response.blob();
-
-    const file = new File([blob], "ipfsFile.png", { type: "image/png" });
-
+  async function sendToIPFS(file) {
     const formData = new FormData();
     formData.append("file", file);
 
@@ -67,8 +60,16 @@ const NewAuction = () => {
     });
 
     const fileHash = `https://ipfs.io/ipfs/${resFile.data.IpfsHash}`;
+  }
 
-    console.log(fileHash);
+  async function fetchImageAndConvertToBase64(url) {
+    // Fetch the image
+    const response = await fetch(url);
+
+    // Get the blob data of the image
+    const blob = await response.blob();
+
+    const file = new File([blob], "ipfsFile.png", { type: "image/png" });
 
     // Function to convert blob to Base64
     const blobToBase64 = (blob) => {
@@ -83,14 +84,13 @@ const NewAuction = () => {
     // Convert blob to Base64
     const base64Data = await blobToBase64(blob);
 
-    const object = {
-      satelliteImage: fileHash,
-      base64Image: base64Data,
-    };
+    setBase64(base64Data as any);
+    setFile(file as any)
 
-    window.localStorage.setItem("object", JSON.stringify(object));
+    console.log("BASE 64", base64)
+    console.log("FILE", file)
 
-    return fileHash;
+    return base64Data;
   }
 
   async function getResult(e: any) {
@@ -110,197 +110,208 @@ const NewAuction = () => {
     setLoaded(true);
   }
 
-  function changePage(e: any, type: string) {
+  async function changePage(e: any, type: string) {
     e.preventDefault();
+    setChangingPage(true)
     if (type == "next") {
-      const object = JSON.parse((window as any).localStorage.getItem("object"));
-
-      if (object) {
-        if (
-          object.satelliteImage == "" ||
-          object.satelliteImage == undefined ||
-          object.satelliteImage == null ||
-          object.base64Image == undefined ||
-          object.base64Image == null ||
-          object.base64Image == ""
-        ) {
-          toast.error("You need to upload a satellite image");
-          return;
-        }
-      } else {
-        toast.error("You need to upload a satellite image");
-        return;
+      if(!file) {
+        toast.error("You need to upload an image")
+        return
       }
+
+      if(!base64) {
+        toast.error("You need to upload an image")
+        return
+      }
+
+      const ipfsLink = await sendToIPFS(file)
+
+      const object = {
+        satelliteImage: ipfsLink,
+        base64: base64,
+      }
+      
+      window.localStorage.setItem("auction", JSON.stringify(object))
 
       setIsFirstPage(false);
       setIsSecondPage(true);
+
+      setChangingPage(false)
     } else {
       setIsFirstPage(true);
       setIsSecondPage(false);
+
+      setChangingPage(false)
     }
   }
 
   return (
     <Layout title={"Auctions"}>
-      <Toaster position="top-center" reverseOrder={false} />
-      <div className="w-auto flex flex-col content-start items-center mt-6 ml-6">
-        <h1 className="w-auto text-3xl self-start font-bold">
-          Create an Auction
-        </h1>
-        <label className="w-auto text-sm self-start font-bold text-gray-400">
-          You could create an auction for your property
-        </label>
-      </div>
-      {isFirstPage && (
-        <div className="flex flex-col md:flex-row justify-center items-center gap-10 md:gap-56 mt-10 md:mt-32">
-          <form className="flex flex-col justify-center items-center mt-6 ml-6">
-            <div className="flex flex-col justify-center items-start">
-              <label className="text-md font-bold">Coordenadas</label>
-              {qntInputs.map((input, index) => (
-                <input
-                  key={index}
-                  className="w-[90%] md:w-[500px] h-[45px] border-2 border-blue-500 px-4 py-4 rounded-xl outline-0 mt-2"
-                  type="text"
-                  placeholder={`Coordenada ${index + 1}`}
-                  onChange={(e) => {
-                    saveCoordinate(e.target.value, index);
-                  }}
-                />
-              ))}
-              {/* <input className='w-[90%] md:w-[500px] h-[45px] border-2 border-blue-500 px-4 py-4 rounded-xl outline-0 mt-2' type='text' placeholder='Rua Teste, 123' /> */}
-              <div className="w-full flex justify-center items-center gap-2 mt-4">
-                <button
-                  onClick={(e) => {
-                    changeQnt(e, 1);
-                  }}
-                  className="w-[30px] h-[30px] flex justify-center items-center rounded-[50%] bg-blue-500"
-                >
-                  <AiOutlinePlus color="white" />
-                </button>
-                <label>ou</label>
-                <button
-                  onClick={(e) => {
-                    changeQnt(e, 0);
-                  }}
-                  className="w-[30px] h-[30px] flex justify-center items-center rounded-[50%] bg-blue-500"
-                >
-                  <AiOutlineMinus color="white" />
-                </button>
-              </div>
-              <div className="w-full flex justify-center items-center mt-8">
-                <button
-                  onClick={(e) => {
-                    getResult(e);
-                  }}
-                  className="w-[200px] h-[40px] text-white font-bold flex justify-center items-center rounded-xl bg-blue-500"
-                >
-                  Processar Coordenadas
-                </button>
+      {
+        changingPage ? (
+          <WaveTopBottomLoading color="#3B82F6" size="large"/>
+        ) : (
+        <>
+          <Toaster position="top-center" reverseOrder={false} />
+          <div className="w-auto flex flex-col content-start items-center mt-6 ml-6">
+            <h1 className="w-auto text-3xl self-start font-bold">
+              Create an Auction
+            </h1>
+            <label className="w-auto text-sm self-start font-bold text-gray-400">
+              You could create an auction for your property
+            </label>
+          </div>
+          {isFirstPage && (
+            <div className="flex flex-col md:flex-row justify-center items-center gap-10 md:gap-56 mt-10 md:mt-32">
+              <form className="flex flex-col justify-center items-center mt-6 ml-6">
+                <div className="flex flex-col justify-center items-start">
+                  <label className="text-md font-bold">Coordenadas</label>
+                  {qntInputs.map((input, index) => (
+                    <input
+                      key={index}
+                      className="w-[90%] md:w-[500px] h-[45px] border-2 border-blue-500 px-4 py-4 rounded-xl outline-0 mt-2"
+                      type="text"
+                      placeholder={`Coordenada ${index + 1}`}
+                      onChange={(e) => {
+                        saveCoordinate(e.target.value, index);
+                      }}
+                    />
+                  ))}
+                  {/* <input className='w-[90%] md:w-[500px] h-[45px] border-2 border-blue-500 px-4 py-4 rounded-xl outline-0 mt-2' type='text' placeholder='Rua Teste, 123' /> */}
+                  <div className="w-full flex justify-center items-center gap-2 mt-4">
+                    <button
+                      onClick={(e) => {
+                        changeQnt(e, 1);
+                      }}
+                      className="w-[30px] h-[30px] flex justify-center items-center rounded-[50%] bg-blue-500"
+                    >
+                      <AiOutlinePlus color="white" />
+                    </button>
+                    <label>ou</label>
+                    <button
+                      onClick={(e) => {
+                        changeQnt(e, 0);
+                      }}
+                      className="w-[30px] h-[30px] flex justify-center items-center rounded-[50%] bg-blue-500"
+                    >
+                      <AiOutlineMinus color="white" />
+                    </button>
+                  </div>
+                  <div className="w-full flex justify-center items-center mt-8">
+                    <button
+                      onClick={(e) => {
+                        getResult(e);
+                      }}
+                      className="w-[200px] h-[40px] text-white font-bold flex justify-center items-center rounded-xl bg-blue-500"
+                    >
+                      Processar Coordenadas
+                    </button>
+                  </div>
+                </div>
+              </form>
+              <div>
+                {loaded ? (
+                  <div className="w-full flex flex-col justify-center items-center">
+                    <label className="w-full text-center font-bold mb-4 text-2xl">
+                      Imagem
+                    </label>
+                    <img
+                      className="border-4 border-blue-500 rounded-2xl"
+                      src={map}
+                    ></img>
+                  </div>
+                ) : (
+                  <div className="flex flex-col items-center justify-center gap-6">
+                    <label>Aguardando Coordenadas</label>
+                    <div className="relative mr-8">
+                      <SemipolarLoading color="#3B82F6" size="small" speed={8} />
+                    </div>
+                  </div>
+                )}
               </div>
             </div>
-          </form>
-          <div>
-            {loaded ? (
-              <div className="w-full flex flex-col justify-center items-center">
-                <label className="w-full text-center font-bold mb-4 text-2xl">
-                  Imagem
-                </label>
-                <img
-                  className="border-4 border-blue-500 rounded-2xl"
-                  src={map}
-                ></img>
+          )}
+
+          {isSecondPage && (
+            <form className="w-full flex flex-col justify-center items-center mt-6 ml-6 gap-4">
+              <div className="flex flex-col justify-center items-start">
+                <label className="text-md font-bold">Título</label>
+                <input
+                  className="w-[90%] md:w-[500px] h-[45px] border-2 border-blue-500 px-4 py-4 rounded-xl outline-0 mt-2"
+                  type="text"
+                  placeholder={`Título`}
+                  // onChange={(e) => {
+                  //     setTitle(e.target.value);
+                  // }}
+                />
               </div>
-            ) : (
-              <div className="flex flex-col items-center justify-center gap-6">
-                <label>Aguardando Coordenadas</label>
-                <div className="relative mr-8">
-                  <SemipolarLoading color="#3B82F6" size="small" speed={8} />
-                </div>
+
+              <div className="flex flex-col justify-center items-start">
+                <label className="text-md font-bold">Description</label>
+                <input
+                  className="w-[90%] md:w-[500px] h-[45px] border-2 border-blue-500 px-4 py-4 rounded-xl outline-0 mt-2"
+                  type="text"
+                  placeholder={`Description`}
+                  // onChange={(e) => {
+                  //     setTitle(e.target.value);
+                  // }}
+                />
               </div>
+
+              <div className="flex flex-col justify-center items-start">
+                <label className="text-md font-bold">State</label>
+                <input
+                  className="w-[90%] md:w-[500px] h-[45px] border-2 border-blue-500 px-4 py-4 rounded-xl outline-0 mt-2"
+                  type="text"
+                  placeholder={`State`}
+                  // onChange={(e) => {
+                  //     setTitle(e.target.value);
+                  // }}
+                />
+              </div>
+            </form>
+          )}
+
+          <div className="w-full mt-24 flex flex-row items-center justify-center gap-2">
+            {isFirstPage && (
+              <>
+                <button
+                  disabled
+                  className="w-[140px] h-[40px] text-white font-bold flex justify-center items-center rounded-xl bg-blue-500 disabled:opacity-50"
+                >
+                  Back Page
+                </button>
+                <button
+                  onClick={(e) => {
+                    changePage(e, "next");
+                  }}
+                  className="w-[140px] h-[40px] text-white font-bold flex justify-center items-center rounded-xl bg-blue-500"
+                >
+                  Next Page
+                </button>
+              </>
+            )}
+
+            {isSecondPage && (
+              <>
+                <button
+                  onClick={(e) => {
+                    changePage(e, "prev");
+                  }}
+                  className="w-[140px] h-[40px] text-white font-bold flex justify-center items-center rounded-xl bg-blue-500"
+                >
+                  Back Page
+                </button>
+                <button
+                  className="w-[140px] h-[40px] text-white font-bold flex justify-center items-center rounded-xl bg-blue-500 disabled:opacity-50"
+                >
+                  Concluir
+                </button>
+              </>
             )}
           </div>
-        </div>
+        </>
       )}
-
-      {isSecondPage && (
-        <form className="w-full flex flex-col justify-center items-center mt-6 ml-6 gap-4">
-          <div className="flex flex-col justify-center items-start">
-            <label className="text-md font-bold">Título</label>
-            <input
-              className="w-[90%] md:w-[500px] h-[45px] border-2 border-blue-500 px-4 py-4 rounded-xl outline-0 mt-2"
-              type="text"
-              placeholder={`Título`}
-              // onChange={(e) => {
-              //     setTitle(e.target.value);
-              // }}
-            />
-          </div>
-
-          <div className="flex flex-col justify-center items-start">
-            <label className="text-md font-bold">Description</label>
-            <input
-              className="w-[90%] md:w-[500px] h-[45px] border-2 border-blue-500 px-4 py-4 rounded-xl outline-0 mt-2"
-              type="text"
-              placeholder={`Description`}
-              // onChange={(e) => {
-              //     setTitle(e.target.value);
-              // }}
-            />
-          </div>
-
-          <div className="flex flex-col justify-center items-start">
-            <label className="text-md font-bold">State</label>
-            <input
-              className="w-[90%] md:w-[500px] h-[45px] border-2 border-blue-500 px-4 py-4 rounded-xl outline-0 mt-2"
-              type="text"
-              placeholder={`State`}
-              // onChange={(e) => {
-              //     setTitle(e.target.value);
-              // }}
-            />
-          </div>
-        </form>
-      )}
-
-      <div className="w-full mt-24 flex flex-row items-center justify-center gap-2">
-        {isFirstPage && (
-          <>
-            <button
-              disabled
-              className="w-[140px] h-[40px] text-white font-bold flex justify-center items-center rounded-xl bg-blue-500 disabled:opacity-50"
-            >
-              Back Page
-            </button>
-            <button
-              onClick={(e) => {
-                changePage(e, "next");
-              }}
-              className="w-[140px] h-[40px] text-white font-bold flex justify-center items-center rounded-xl bg-blue-500"
-            >
-              Next Page
-            </button>
-          </>
-        )}
-
-        {isSecondPage && (
-          <>
-            <button
-              onClick={(e) => {
-                changePage(e, "prev");
-              }}
-              className="w-[140px] h-[40px] text-white font-bold flex justify-center items-center rounded-xl bg-blue-500"
-            >
-              Back Page
-            </button>
-            <button
-              disabled
-              className="w-[140px] h-[40px] text-white font-bold flex justify-center items-center rounded-xl bg-blue-500 disabled:opacity-50"
-            >
-              Next Page
-            </button>
-          </>
-        )}
-      </div>
     </Layout>
   );
 };
